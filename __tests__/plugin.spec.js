dce2a640c965be48e33a2c73c79c811700776ec7
Stanislav Termosa <termosa.stanislav@gmail.com>
2018-03-20 07:49:56 +0200

    cover plugin with tests

---
+++ __tests__/plugin.spec.js
@@ -0,0 +1,28 @@
+const plugin = require('../src/plugin')
+
+describe('Cli Plugin', () => {
+  it('exists', () => {
+    expect(typeof plugin.install).toBe('function')
+    expect(typeof plugin.CliPlugin).toBe('function')
+    expect(plugin.install).toBe(plugin.CliPlugin)
+    expect(plugin.install()).toHaveProperty('executeCommand')
+    expect(plugin.install()).toHaveProperty('registerCommand')
+    expect(plugin.install()).toHaveProperty('getCommands')
+    expect(() => plugin.install({})).not.toThrow()
+  })
+
+  it('export API', () => {
+    const obj = {}
+    plugin.install(obj)
+
+    expect(typeof obj.executeCommand).toBe('function', 'exports executeCommand()')
+    expect(typeof obj.registerCommand).toBe('function', 'exports registerCommand()')
+    expect(typeof obj.getCommands).toBe('function', 'exports getCommands()')
+  })
+
+  it('receive empty list of commands', () => {
+    const commands = plugin.install().getCommands()
+    expect(Array.isArray(commands)).toBeTruthy()
+    expect(commands.length).toBe(0)
+  })
+})
