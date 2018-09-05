22c148924a221db66ad2d42c616cfec736ea1dd8
Stanislav Termosa <termosa.stanislav@gmail.com>
2018-09-05 22:45:36 +0300

    add cli.allowShort option

--- __tests__/normalize-options.spec.js
+++ __tests__/normalize-options.spec.js
@@ -6,7 +6,7 @@ describe('normalizeOptions()', () => {
   })
 
   it('returns default options', () => {
-    expect(normalizeOptions({})).toEqual({ guide: {} })
+    expect(normalizeOptions({})).toEqual({ cli: {}, guide: {} })
   })
 
   it('fails if options is not an object', () => {
@@ -14,35 +14,64 @@ describe('normalizeOptions()', () => {
       .toThrow('options argument should be defined as an object')
   })
 
-  it('adds firstMessage to guide property', () => {
-    expect(normalizeOptions({ guide: { firstMessage: 'hello' } }))
-      .toEqual({ guide: { firstMessage: 'hello' } })
+  describe('cli', () => {
+    it('works properly when cli is an empty object', () => {
+      expect(normalizeOptions({ cli: {} }))
+        .toEqual({ cli: {}, guide: {} })
+    })
+
+    it('adds allowShort to cli property', () => {
+      expect(normalizeOptions({ cli: { allowShort: true } }))
+        .toEqual({ cli: { allowShort: true }, guide: {} })
+    })
+
+    it('adds allowShort to cli property even if it is falsy', () => {
+      expect(normalizeOptions({ cli: { allowShort: false } }))
+        .toEqual({ cli: { allowShort: false }, guide: {} })
+    })
+
+    it('ignores cli if it is not an object', () => {
+      expect(normalizeOptions({ cli: 42 }))
+        .toEqual({ cli: {}, guide: {} })
+    })
   })
 
-  it('ignores firstMessage if it is not a string', () => {
-    expect(normalizeOptions({ guide: { firstMessage: 42 } }))
-      .toEqual({ guide: {} })
-  })
+  describe('guide', () => {
+    it('works properly when guide is an empty object', () => {
+      expect(normalizeOptions({ guide: {} }))
+        .toEqual({ cli: {}, guide: {} })
+    })
 
-  it('adds beforeCallback to guide property if guide is set to function', () => {
-    const cb = () => {}
-    expect(normalizeOptions({ guide: cb }))
-      .toEqual({ guide: { beforeCallback: cb } })
-  })
+    it('adds firstMessage to guide property', () => {
+      expect(normalizeOptions({ guide: { firstMessage: 'hello' } }))
+        .toEqual({ cli: {}, guide: { firstMessage: 'hello' } })
+    })
 
-  it('adds beforeCallback to guide property', () => {
-    const cb = () => {}
-    expect(normalizeOptions({ guide: { beforeCallback: cb } }))
-      .toEqual({ guide: { beforeCallback: cb } })
-  })
+    it('ignores firstMessage if it is not a string', () => {
+      expect(normalizeOptions({ guide: { firstMessage: 42 } }))
+        .toEqual({ cli: {}, guide: {} })
+    })
 
-  it('ignores beforeCallback if it is not a function', () => {
-    expect(normalizeOptions({ guide: { beforeCallback: 42 } }))
-      .toEqual({ guide: {} })
-  })
+    it('adds beforeCallback to guide property if guide is set to function', () => {
+      const cb = () => {}
+      expect(normalizeOptions({ guide: cb }))
+        .toEqual({ cli: {}, guide: { beforeCallback: cb } })
+    })
 
-  it('ignores guide if it is not an object or a function', () => {
-    expect(normalizeOptions({ guide: 42 }))
-      .toEqual({ guide: {} })
+    it('adds beforeCallback to guide property', () => {
+      const cb = () => {}
+      expect(normalizeOptions({ guide: { beforeCallback: cb } }))
+        .toEqual({ cli: {}, guide: { beforeCallback: cb } })
+    })
+
+    it('ignores beforeCallback if it is not a function', () => {
+      expect(normalizeOptions({ guide: { beforeCallback: 42 } }))
+        .toEqual({ cli: {}, guide: {} })
+    })
+
+    it('ignores guide if it is not an object or a function', () => {
+      expect(normalizeOptions({ guide: 42 }))
+        .toEqual({ cli: {}, guide: {} })
+    })
   })
 })
